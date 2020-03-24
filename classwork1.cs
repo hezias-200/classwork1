68d589517db53fcf1eacc80baa610f95e71d3d90
hezias-200 <62279337+hezias-200@users.noreply.github.com>
2020-03-24 12:30:58 +0200

    Add files via upload

---
+++ classwork1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Classwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose 2 numbers:");//Print on the screen ----> Choose 2 numbers:
+            int lengthOfarray = Convert.ToInt32(Console.ReadLine());//I defined variable and choose my number from the console and convert him to integer
+            int numOfmull = Convert.ToInt32(Console.ReadLine());//I defined variable and choose my number from the console and convert him to integer  
+            int[] arr2 = new int[lengthOfarray];//I defined array in size of number that i chose before ,in name "lengthOfarray"
+            for (int i = 0; i < arr2.Length; i++)//הרצתי לולאה עד גודל המערך 
+            {
+                arr2[i] = i * numOfmull;//הגדרתי שהאיבר במקום האינדקס יהיה שווה לאינדקס כפול המספר שבחרתי להכפיל לפני בשם "numOfmull"
+
+
+                Console.Write(arr2[i] + " , ");//מדפיס את המערך עם פסיקים ורווחים
+            }
+            Console.WriteLine("\n");//ירדתי שורה
+            Console.WriteLine($"num of elements:{lengthOfarray}"); //הדפסתי את המשתנה הראשון שהוא בעצם גודל המערך
+            Console.WriteLine($"num of elements:{numOfmull}");//הדפסתי את המשתנה השני שהוא בעצם הכפלת של האינדקסים 
+            Console.ReadKey();
+        }
+    }
+}
